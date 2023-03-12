7e2f1e74842f7f4ca389db4aceca890c2c9c586e
S00210475 <s00210475@atu.ie>
2023-03-12 15:18:29 +0000

    Error with libpenguin.so

--- app/src/main/java/com/example/safetycheck/MainActivity.java
+++ app/src/main/java/com/example/safetycheck/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.safetycheck;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class MainActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private Button submitButton;
     private TextView remainingTimeTextView;
-    private int remainingTime = 30;
+    private int remainingTime;
     private CountDownTimer timer;
 
     @Override
@@ -27,14 +38,18 @@ public class MainActivity extends AppCompatActivity {
         submitButton = findViewById(R.id.Submit);
         remainingTimeTextView = findViewById(R.id.remainingTimeTextView);
 
-        timer = new CountDownTimer(30000, 1000) {
+        timer = new CountDownTimer(5000, 1000) {
             public void onTick(long millisUntilFinished) {
                 remainingTime = (int) millisUntilFinished / 1000;
                 remainingTimeTextView.setText("Remaining Time: " + remainingTime);
             }
 
             public void onFinish() {
-                sendEmail();
+                new Thread(new Runnable() {
+                    public void run() {
+                        sendEmail();
+                    }
+                }).start();
             }
         }.start();
 
@@ -52,21 +67,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void sendEmail() {
-        String email = "chriscurran01@gmail.com";
-        String subject = "Safety Check Failed";
-        String message = "You didn't make your safety check";
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("message/rfc822");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { email });
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        intent.putExtra(Intent.EXTRA_TEXT, message);
+    private void sendEmail() {
+        final String username = "safetycheckapplication@gmail.com";
+        final String password = "GDSC2023";
+        String to = "safetycheckapplication@gmail.com";
+        String subject = "User did not make their safety check";
+        String message = "This is test code for safety check";
 
         try {
-            startActivity(Intent.createChooser(intent, "Send email using..."));
-        } catch (android.content.ActivityNotFoundException ex) {
-            Toast.makeText(MainActivity.this, "No email clients installed.", Toast.LENGTH_SHORT).show();
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props,
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+            MimeMessage msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress(username));
+            msg.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(to));
+            msg.setSubject(subject);
+            msg.setText(message);
+
+            Transport.send(msg);
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MessagingException e) {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Error sending email", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
