a210d73fb26fd01d52613729b056dbb36561ae91
S00210475 <s00210475@atu.ie>
2023-03-30 15:54:20 +0100

    New Button Input

--- app/src/main/java/com/example/safetycheck/MainActivity.java
+++ app/src/main/java/com/example/safetycheck/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.safetycheck;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -11,9 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.Properties;
-
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
@@ -23,21 +20,27 @@ import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
     private EditText passwordEditText;
-    private Button submitButton;
+    private Button submitButton, buttonOne, buttonTwo, buttonThree, buttonFour;
     private TextView remainingTimeTextView;
-    private int remainingTime;
+    private int remainingTime, startingTime = 60000;
     private CountDownTimer timer;
+    String password ="", correctPassword = "1234";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.newactivity);
+        Log.i("Test", "Buttons being found");
 
-        passwordEditText = findViewById(R.id.PasswordEditText);
-        submitButton = findViewById(R.id.Submit);
+        buttonOne = findViewById(R.id.btn1);
+        buttonTwo = findViewById(R.id.btn2);
+        buttonThree = findViewById(R.id.btn3);
+        buttonFour = findViewById(R.id.btn4);
         remainingTimeTextView = findViewById(R.id.remainingTimeTextView);
+        submitButton = findViewById(R.id.submit);
+        Log.i("Test", "Buttons found");
 
-        timer = new CountDownTimer(5000, 1000) {
+        timer = new CountDownTimer(startingTime, 1000) {
             public void onTick(long millisUntilFinished) {
                 remainingTime = (int) millisUntilFinished / 1000;
                 remainingTimeTextView.setText("Remaining Time: " + remainingTime);
@@ -52,17 +55,46 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
+
+
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String password = passwordEditText.getText().toString();
 
-                if (password.equals("correctpassword")) {
+                if (password.equals(correctPassword)) {
                     timer.cancel();
+                    timer.start();
                     Toast.makeText(MainActivity.this, "Password Correct", Toast.LENGTH_SHORT).show();
+                    password = "";
                 } else {
+                    Log.i("Test", password);
                     Toast.makeText(MainActivity.this, "Password Incorrect", Toast.LENGTH_SHORT).show();
+                    password = "";
                 }
+            }
+        });
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password = password + "1";
+            }
+        });
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password = password + "2";
+            }
+        });
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password = password + "3";
+            }
+        });
+        buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password = password + "4";
             }
         });
     }
@@ -70,10 +102,10 @@ public class MainActivity extends AppCompatActivity {
     private void sendEmail() {
         Log.i("Test", "Send Email Start");
         String host="mail.javatpoint.com";
-        final String user="safetycheckapplication@gmail.com";//change accordingly
-        final String password="GDSC2023";//change accordingly
+        final String user="";//change accordingly
+        final String password="";//change accordingly
 
-        String to="chriscurran01@gmail.com";//change accordingly
+        String to="";//change accordingly
         Log.i("Test", "Properties Start");
         //Get the session object
         Properties props = new Properties();
